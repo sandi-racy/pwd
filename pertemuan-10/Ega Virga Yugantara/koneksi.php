9a4b948c6dbe535fe25d4aa2c67abde1f35fa378
evyugantara <egavirga01@gmail.com>
2025-05-28 21:27:36 +0700

    Tugas pertemuan 10

---
+++ pertemuan-10/Ega Virga Yugantara/koneksi.php
@@ -0,0 +1,14 @@
+
+<?php
+$host = "localhost";
+$port = "3307";
+$user = "root";
+$pass = ""; // isi kalau kamu pakai password
+$dbname = "kampus_db";
+
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
